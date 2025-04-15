bbf6ec92f913d27b93494e1933e14f246d68022d
Walter Mikula <146771447+walterMikula@users.noreply.github.com>
2025-04-15 16:49:17 -0500

    Update Gameproject.jsx

--- src/Gameproject.jsx
+++ src/Gameproject.jsx
@@ -7,7 +7,7 @@ const FlappyBirdGame = () => {
   const BIRD_HEIGHT = 30;
   const PIPE_WIDTH = 60;
   const GRAVITY = 0.5;
-  const JUMP_FORCE = -5;
+  const JUMP_FORCE = -10;
   const DROP_FORCE = 5; // Force for pressing down key
   const PIPE_GAP = 170;
   const PIPE_SPEED = 2;
